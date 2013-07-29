1790013770a413e67792adf778e96033022dc1af
Shockk <shokku.ra@gmail.com>
2013-07-29 23:07:50 +0000

    Added handler for phppath/php (safe=phppathphp).

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ var server = new emweb.Server();
 server.routes.default = 'home.w';
 server.routes[404] = '404.w';
 
+server.routes.phppathphp = function(request, response, request_url, data)
+{
+	response.setHeader('Content-Type', 'text/plain');
+
+	/*
+	response.setHeader('Content-Encoding', 'gzip');
+
+	var i = 0;
+	var s = 'No. ';
+	var fn = function()
+	{
+		//response.write(server.file_cache['1000mb.gz']);
+		response.write(s[i++ % s.length]);
+
+		setTimeout(fn, 200);
+	}
+
+	fn();
+	return false;
+	*/
+
+	response.write('No.');
+};
+
 server.content_types.png = 'image/png';
 server.content_types.css = 'text/css';
 
